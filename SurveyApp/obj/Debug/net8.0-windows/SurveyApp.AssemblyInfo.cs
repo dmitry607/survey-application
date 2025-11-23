e5048eab16a2153b2dcdae187ec3820b475877e9
Dmitry <ivanovdmitry05@mail.ru>
2025-11-24 01:50:48 +0400

    WPF Survey App

--- SurveyApp/obj/Debug/net8.0-windows/SurveyApp.AssemblyInfo.cs
+++ SurveyApp/obj/Debug/net8.0-windows/SurveyApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SurveyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2846ca33e8989c78f61c4cd6f95013ec208bcf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7930bd0bc22956754305875810b85253c68bb95f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SurveyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SurveyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
